47489e00775333526541d2c560a540bd9aa01ccb
RM <stripo420@gmail.com>
2023-10-29 20:33:51 +0000

    Testing completed pipeline.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testt'));
+app.get('/', (req, res) => res.send('Tarrare test. Line is now piped.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
